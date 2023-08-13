fdc171577420af9358110af1b1e3ffed42b42ab6
Enes YARDIM <enes.yardim@saritay.com>
2023-08-13 11:52:01 +0300

    initial commit

---
+++ src/main/java/com/example/springsecuritydemoapp/HelloController.java
@@ -0,0 +1,4 @@
+package com.example.springsecuritydemoapp;
+
+public class HelloController {
+}
---
+++ src/main/java/com/example/springsecuritydemoapp/SpringSecurityDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritydemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDemoAppApplication.class, args);
+    }
+
+}
